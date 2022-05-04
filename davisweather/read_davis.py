975be3ea2800dedf6997ce628b80496bab9bf5e7
ee-quipment.com <support@ee-quipment.com>
2022-05-04 14:00:22 -0700

    update

--- davisweather/read_davis.py
+++ davisweather/read_davis.py
@@ -1,11 +1,31 @@
 import json
 import datetime
 
+
+WIND_BINS = 31
+DIR_BINS  = 8
+
+def bin(value):
+    max_bin = WIND_BINS - 1
+    return(max_bin if (int(value) >= max_bin) else int(value))
+
+def ordinal(dir):
+    SECTOR = (337, 23, 68, 112, 157, 202, 247, 292, 337)
+    if dir > SECTOR[0] or dir <= SECTOR[1]: return (0)
+    for o in range(1, 8):
+        if dir > SECTOR[o] and dir <= SECTOR[o+1]:
+            return (o)
+
+
 f = open("davis")
 linenum = 1
 start_time = 0
 
-hourly_wind = [0] * 24
+# [hour][wind_speed]
+hourly_wind = [[0 for i in range(WIND_BINS)] for j in range(24)]
+hourly_dir = [[0 for i in range(DIR_BINS)] for j in range(24)]
+hourly_gust = [[0 for i in range(WIND_BINS)] for j in range(24)]
+hourly_gust_dir = [[0 for i in range(DIR_BINS)] for j in range(24)]
 
 jline = f.readline()
 while (jline):
@@ -18,19 +38,30 @@ while (jline):
     if start_time == 0: start_time = ts
 
 
-    hour = datetime.datetime.fromtimestamp(ts).strftime("%H")
+    hour = int(datetime.datetime.fromtimestamp(ts).strftime("%H"))
 
     min = (ts - start_time) / 60
     wind_avg = data["data"]["conditions"][0]["wind_speed_avg_last_10_min"]
     wind_dir = data["data"]["conditions"][0]["wind_dir_scalar_avg_last_10_min"]
     gust = data["data"]["conditions"][0]["wind_speed_hi_last_10_min"]
     gust_dir = data["data"]["conditions"][0]["wind_dir_at_hi_speed_last_10_min"]
-    print ("%d, %s, %s, %s, %s" % (min, wind_avg, wind_dir, gust, gust_dir))
+
+    hourly_wind[hour][bin(wind_avg)] += 1
+    hourly_dir[hour][ordinal(wind_dir)] += 1
+    hourly_gust[hour][bin(gust)] += 1
+    hourly_gust_dir[hour][ordinal(gust_dir)] += 1
+
     jline = f.readline()
     linenum += 1
 
 f.close()
-hour = datetime.datetime.fromtimestamp(start_time).strftime("%H")
-print(hour, datetime.datetime.fromtimestamp(start_time))
+
+for h in range(24):
+    avg = 0
+    high = 0
+    for i in range(WIND_BINS):
+        avg += i * hourly_wind[h][i]
+        high = i if (hourly_gust[h][i] and i > high) else high
+    print (avg / (WIND_BINS-1), ',', high)
 
 
