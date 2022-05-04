e7ff5ce2919fbfd53a6e14079eb6601757020de1
ee-quipment.com <support@ee-quipment.com>
2022-05-03 17:36:35 -0700

    update

--- davisweather/read_davis.py
+++ davisweather/read_davis.py
@@ -1,7 +1,11 @@
 import json
+import datetime
 
 f = open("davis")
 linenum = 1
+start_time = 0
+
+hourly_wind = [0] * 24
 
 jline = f.readline()
 while (jline):
@@ -9,19 +13,24 @@ while (jline):
         data = json.loads(jline[:-3])
     except:
         print (linenum, "exception, data = ", len(data), "\n\n\n\r")
-        print (jline)
-        print (data['data'])
-        for key, value in data:
-            print(key, value)
-        break
-    otmp = data["data"]["conditions"][0]["temp"]
-    ohum = data["data"]["conditions"][0]["hum"]
-    itmp = data["data"]["conditions"][1]["temp_in"]
-    ihum = data["data"]["conditions"][1]["hum_in"]
-    #print (linenum, otmp, ohum, itmp, ihum)
+
+    ts = data["data"]["ts"]
+    if start_time == 0: start_time = ts
+
+
+    hour = datetime.datetime.fromtimestamp(ts).strftime("%H")
+
+    min = (ts - start_time) / 60
+    wind_avg = data["data"]["conditions"][0]["wind_speed_avg_last_10_min"]
+    wind_dir = data["data"]["conditions"][0]["wind_dir_scalar_avg_last_10_min"]
+    gust = data["data"]["conditions"][0]["wind_speed_hi_last_10_min"]
+    gust_dir = data["data"]["conditions"][0]["wind_dir_at_hi_speed_last_10_min"]
+    print ("%d, %s, %s, %s, %s" % (min, wind_avg, wind_dir, gust, gust_dir))
     jline = f.readline()
     linenum += 1
 
 f.close()
+hour = datetime.datetime.fromtimestamp(start_time).strftime("%H")
+print(hour, datetime.datetime.fromtimestamp(start_time))
 
 
